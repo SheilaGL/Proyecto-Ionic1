90a03a8c20e7b8cd34de8781b677fc3835df8e07
Sheila Guadiño <sheila.guadino@external.wolterskluwer.com>
2020-03-31 18:20:58 +0200

    Crear un atributo de entrada en el componente al que le llegue una lista de personas.

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,11 +1,30 @@
 import {Component, Input} from '@angular/core';
 
+
 @Component({
     selector: 'app-list',
     templateUrl: 'list.component.html'
 })
 
+
 export class ListComponent {
     @Input() title: string = 'Titulo de lista no definida';
+
+
+    list: Array<any> = [
+        {
+            name: 'Branko Fuenzalida'
+        },
+        {
+            name: 'Santi Figueiras'
+        },
+        {
+            name: 'Sheila Guadiño'
+        },
+        {
+            name: 'Cristina Romero'
+        }
+
+    ];
 
 }
