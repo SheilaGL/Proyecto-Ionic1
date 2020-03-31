de1c7eed038f42083ca2f463c190e643c47af52b
Sheila Guadiño <sheila.guadino@external.wolterskluwer.com>
2020-03-31 17:57:35 +0200

    Bindeado el título de la lista con un @Input en el componente. Modificación en tslint.json "no-inferrable-types" a false

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
     selector: 'app-list',
@@ -6,5 +6,6 @@ import {Component} from '@angular/core';
 })
 
 export class ListComponent {
-  public title = 'List Component';
+    @Input() title: string = 'Titulo de lista no definida';
+
 }
